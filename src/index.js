c30e7d8fdc2ea309e34ac49f2ce7d83da3466ac3
Stuart Casarotto <stuartcasarotto@stuarts-mbp.lan>
2018-10-26 15:28:48 -0500

    updated readme

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import * as serviceWorker from './serviceWorker'
